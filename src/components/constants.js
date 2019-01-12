4f8b616bd64143589d6be9cc91edc0613966a942
Kevin Kang <kevink97@cs.washington.edu>
2019-01-12 15:54:14 -0800

    added dependencies

---
+++ src/components/constants.js
@@ -0,0 +1,13 @@
+/*
+ * @flow strict-local
+ */
+
+export type SiteTab = "goals" | "blog" | "events" | "contact" | "register";
+
+export const SiteTabValue = {
+  goals: "Goals",
+  blog: "Blog",
+  events: "Events",
+  contact: "Contact Us",
+  register: "Register"
+};
