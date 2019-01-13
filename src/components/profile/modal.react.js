dd620ebff40c6977829b4a895a95c216a0f3a523
Laurissa Lam <laurissa@Laurs-Mac.local>
2019-01-13 04:24:16 -0800

    modal

--- src/components/profile/modal.react.js
+++ src/components/profile/modal.react.js
@@ -6,7 +6,12 @@ type Props = {
   onModalClose: () => void,
 };
 
-class ModalClass extends React.Component<Props> {
+type State = { org: string}
+
+class ModalClass extends React.Component<Props, State> {
+  state = {
+    org: ""
+  };
   constructor(props) {
     super(props);
     this.state = {
@@ -14,15 +19,23 @@ class ModalClass extends React.Component<Props> {
     };
 
     // toggle = this.toggle.bind(this);
-}
+  }
+
+  
 
   render() {
     return (
       <div>
-        <Modal isOpen={this.state.modal} toggle={this.modalToggled}>
+        <Modal isOpen={this.modalToggled} toggle={this.modalToggled}>
           <ModalHeader toggle={this.modalToggled}>Modal title</ModalHeader>
           <ModalBody>
-            YO DIS THE MEMBERSHIP CARD
+            <div class="card" style = {{width: 50 + '%'}}>
+            <div class="card-body">
+              <h4 class="card-title" style = {{marginTop: 2 + 'rem'}}>{this.state.org}</h4>
+              <h4 class="card-text" style = {{marginTop: 5 + 'rem'}}>Jane Doe</h4>
+              <p class="card-text text-muted" style = {{marginBottom: 1 + 'rem'}}>Valid thru 06/20</p>
+            </div>
+          </div>
           </ModalBody>
         </Modal>
       </div>
